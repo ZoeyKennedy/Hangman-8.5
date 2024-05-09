57af58f4b55b2ebd9342635856466648f5282eb3
Zoey Kennedy <339523656@GoTVDSB.ca>
2024-05-09 14:13:02 -0400

    save

--- Program.cs
+++ Program.cs
@@ -1,14 +1,100 @@
-﻿namespace Hangman_8._5
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace Hangman_8._5
 {
     internal class Program
     { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            art();
+
             int hangman;
+            hangman = 0;
+            string guess;
+            string assignSecretWord;
+            string secretWord;
+            Random rand = new Random();
+            int randomNum;
+            Console.WriteLine("Type a secret word that your friend will guess.");
+            Console.WriteLine("If you are playing alone, type '1' for a random word.");
+            assignSecretWord = Console.ReadLine();
+
+            
 
 
-           
+            if (assignSecretWord == "1")
+            {
+                randomNum = rand.Next(1, 10);
+
+                switch (randomNum)
+                {  
+                    case 1:
+                        secretWord = "";
+                        break;
+
+                        case 2:
+                        secretWord = "";
+                        break; 
+
+                    case 3:
+                        secretWord = "";
+                        break;
+
+                    case 4:
+                        secretWord = "";
+                        break;
+
+                        case 5:
+                        secretWord = "";
+                        break;
+
+                        case 6:
+                        secretWord = "";
+                        break;
+
+                        case 7:
+                        secretWord = "";
+                        break;
+
+                        case 8:
+                        secretWord = "";
+
+                        break;
+
+
+                        case 9:
+                        secretWord = "";
+                        break;
+
+                        case 10:
+                        secretWord = "";
+
+                        break;
+
+                }
+            }
+
+            else
+            {
+                secretWord = assignSecretWord; 
+
+            }
+
+            Console.ReadLine();
+            while (hangman >= 6)
+            { Console.WriteLine("Guess a letter");
+                Console.WriteLine();
+                guess = Console.ReadLine();
+               
+                if (secretWord.Contains(guess))
+                {
+                    Console.WriteLine();
+                }
+            }
+
         }
 
         public static void art()
@@ -17,7 +103,7 @@
             int hangman;
             hangman = 0;
 
-           while (hangman >= 7)
+            if (hangman <= 6)
             {
                 switch (hangman)
                 {
@@ -54,7 +140,7 @@
                             Console.WriteLine("      +---+"); 
                             Console.WriteLine("      |   |"); 
                             Console.WriteLine("      O   |"); 
-                            Console.WriteLine("     /|\  |"); 
+                            Console.WriteLine("     /|/  |"); 
                             Console.WriteLine("          |"); 
                             Console.WriteLine("          |"); 
                             Console.WriteLine("      ========="); 
@@ -64,7 +150,7 @@
                             Console.WriteLine("      +---+"); 
                             Console.WriteLine("      |   |"); 
                             Console.WriteLine("      O   |"); 
-                            Console.WriteLine("     /|\  |"); 
+                            Console.WriteLine("     /|/  |"); 
                             Console.WriteLine("     /    |"); 
                             Console.WriteLine("          |"); 
                             Console.WriteLine("      ========="); 
@@ -76,8 +162,8 @@
                             Console.WriteLine("      +---+"); 
                             Console.WriteLine("      |   |"); 
                             Console.WriteLine("      O   |"); 
-                            Console.WriteLine("     /|\  |"); 
-                            Console.WriteLine("     / \  |"); 
+                            Console.WriteLine("     /|/  |"); 
+                            Console.WriteLine("     / /  |"); 
                             Console.WriteLine("          |"); 
                             Console.WriteLine("      ========="); 
                             break;
@@ -86,16 +172,21 @@
                             Console.WriteLine("      +---+"); 
                             Console.WriteLine("      |   |"); 
                             Console.WriteLine("          |"); 
-                            Console.WriteLine("     \O/  |"); 
+                            Console.WriteLine("     /O/  |"); 
                             Console.WriteLine("      |   |"); 
-                            Console.WriteLine("     / \  |"); 
-                            Console.WriteLine("      ========="); 
+                            Console.WriteLine("     / /  |"); 
+                            Console.WriteLine("      =========");
+                        Console.WriteLine();
+                        Console.WriteLine("You lost!");
                             break;
 
-
-
-
                 }
+
+                
+
+
+
+
 
             }
 
